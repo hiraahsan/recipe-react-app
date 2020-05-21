615649a880dc27a53755b51d1ad8e1d7fbdda347
Hira <hiraahsan94@gmail.com>
2020-05-21 00:36:53 -0400

    mapping over data to display on page

--- src/components/DisplayResults.js
+++ src/components/DisplayResults.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
-const DisplayResults = () => {
+const DisplayResults = (props) => {
+    const {isLoading, data} = props
+    const handleClick = () => {
+        console.log('hello there')
+    }
     return(
-        <div></div>
+        <div>
+            {
+            data.length > 0
+            ? 
+            data.map((item) => {
+            return             <button onClick={handleClick}>
+
+            <img src={item.image} alt={item.title} /> <h2>{item.title}</h2> 
+            <p>Missed ingredients: {item.missedIngredients.map(element => {
+                return element.name + ' '
+                })}</p>
+        </button>
+        })
+            :
+            <p>No data yet</p>
+            }
+        </div>
     )
 }
 export default DisplayResults;
