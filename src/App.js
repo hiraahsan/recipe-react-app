8af1c2daec08c59e902694d5f3732f2857857215
Hira <hiraahsan94@gmail.com>
2020-05-21 21:26:45 -0400

    adding router and component paths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navigation from './components/Navigation';
+import About from './components/About';
 import './App.css';
 
 function App() {
   return (
-    <>
-    <Dashboard />
-    </>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path="/" component={Dashboard} exact/>
+        <Route path="/about" component={About} exact />
+
+      </Switch>
+    </Router>
   );
 }
 
