ddccedf9fe8b519f1d3e376fa99dba21e8165198
Hira <hiraahsan94@gmail.com>
2020-05-23 04:23:52 -0400

    updating to Link router and passing props to single recipe pages

--- src/components/DisplayResults.js
+++ src/components/DisplayResults.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const DisplayResults = (props) => {
     const {data} = props
-    const handleClick = () => {
-        console.log('hello there')
-    }
+    const [id, setId] = useState('');
     return(
         <div>
             {
             data.length > 0
             ? 
             data.map((item) => {
-            return             <button onClick={handleClick}>
+            return             <Link to={{
+
+            pathname:`/recipes/${item.id}`,
+            state: {
+                image: item.image,
+                title: item.title,
+                usedIngredients: item.usedIngredients,
+                missedIngredients: item.missedIngredients
+            }
+            }}>
+            <button onClick={() => setId(item.id)}>
 
             <img src={item.image} alt={item.title} /> <h2>{item.title}</h2> 
             <p>Missed ingredients: {item.missedIngredients.map(element => {
                 return element.name + ' '
                 })}</p>
         </button>
+        </Link>
         })
             :
             <p>No data yet</p>
