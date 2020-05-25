87e9f55662ffacedb59fcadc6c994d48526c2393
Hira <hiraahsan94@gmail.com>
2020-05-25 03:25:12 -0400

    adding user context for Login, updating with class name for nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import UserContext from './UserContext';
 import {NavLink} from 'react-router-dom';
 
 const Navigation = () => {
+    const {user} = useContext(UserContext)
     return(
-        <div>
+        <div className="nav-bar">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/contact">Contact</NavLink>
-            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/login">{user ? "Logout" : "Login"}</NavLink>
         </div>
     )
 }
