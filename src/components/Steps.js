4c2c7caccc29850a745e18c2e70727a532cda599
Hira <hiraahsan94@gmail.com>
2020-05-25 18:07:13 -0400

    removing extraneous code

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,20 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const Steps = (props) => {
     const {steps} = props
-    const [instructions, setInstructions] = useState([]);
-    // useEffect(() => {
-    //     for(let i=0; i<steps.length; i++) {
-    //         console.log(steps[i])
-    //         for(let j=0; j<steps[i].length; j++) {
-    //             console.log(steps[i][j])
-    //             setInstructions(instructions => [...instructions, steps[i][j].step])
-    //         }
-    //     }
-    // }, [steps])
+
     return(
         <>
-        <p>These are the steps</p>
         {steps}
         </>
     )
