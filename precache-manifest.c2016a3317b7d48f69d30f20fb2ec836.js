918ff7daf472e746c772223a24f20f3aee0544e7
Hira <hiraahsan94@gmail.com>
2020-05-25 03:52:52 -0400

    Updates

---
+++ precache-manifest.c2016a3317b7d48f69d30f20fb2ec836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5461f5afca61129a5da73397e872a50d",
+    "url": "/recipe-react-app/index.html"
+  },
+  {
+    "revision": "ff7ab85a66faa443479b",
+    "url": "/recipe-react-app/static/css/main.0462c740.chunk.css"
+  },
+  {
+    "revision": "39a4eaad34deaf67e059",
+    "url": "/recipe-react-app/static/js/2.74c3b25b.chunk.js"
+  },
+  {
+    "revision": "2197e867d978872afb824139f7f0a37a",
+    "url": "/recipe-react-app/static/js/2.74c3b25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7ab85a66faa443479b",
+    "url": "/recipe-react-app/static/js/main.d9897087.chunk.js"
+  },
+  {
+    "revision": "1c773baaa1ebe303eacc",
+    "url": "/recipe-react-app/static/js/runtime-main.849755cc.js"
+  }
+]);
