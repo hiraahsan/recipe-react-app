fde31633410628b3b3c91bf412bdb49f4c6c8e88
Hira <hiraahsan94@gmail.com>
2020-05-25 03:23:19 -0400

    cleaning up lil bit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Dashboard from './components/Dashboard';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navigation from './components/Navigation';
 import About from './components/About';
 import SingleRecipe from './components/SingleRecipe';
-import Login from './components/Login';
-import firebase from './components/firebase';
-import './App.css';
+import LoginPage from './components/LoginPage';
+import './styles/App.scss';
 
-const auth = firebase.auth();
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [name, setName] = useState(null);
-  const userSetup = (assignUser, email, name) => {
-    setUser(assignUser)
-    setEmail(email)
-    setName(name)
-}
-
-const logout = () => {
-  auth.signOut()
-  .then(() => {
-      setUser(null)
-      setEmail(null)
-      setName(null)
-  })
-}
 
   return (
     <Router>
@@ -37,8 +18,9 @@ const logout = () => {
         <Route path="/about" component={About} exact />
         <Route path="/recipes/:id" component={SingleRecipe} />
         <Route path="/login" 
-        // component={Login} 
-        render={(props) => <Login {...props} userSetup={userSetup} />}
+        // component={LoginPage} 
+        render={props => <LoginPage {...props} />
+        }
         />
 
       </Switch>
