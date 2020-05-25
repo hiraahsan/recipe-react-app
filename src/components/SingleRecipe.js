48408ec5507fe55b49ffe5572281decc6ccba3ba
Hira <hiraahsan94@gmail.com>
2020-05-25 03:32:58 -0400

    removing extraneous code

--- src/components/SingleRecipe.js
+++ src/components/SingleRecipe.js
@@ -4,36 +4,23 @@ import {apiKey} from './utils';
 import Steps from './Steps';
 
 const SingleRecipe = (props) => {
-    const [id, setId] = useState()
+    const [id, setId] = useState('')
     const [url, setUrl] = useState('')
     const [data, setData] = useState();
     const [instructions, setInstructions] = useState([]);
-    const [missedIngredients, setMissedIngredients] = useState();
 
 
     const {match, location} = props
-    // for(let i=0; i<location.state.missedIngredients.length; i++) {
-    //     console.log(location.state.missedIngredients[i].name)
-    // }
+    const {state} = location
 
-    useEffect( () => {
-        if (data) {
-            for(let i = 0; i < data.length; i++) {
-                const steps = data[i].steps
-                for (let j=0; j<steps.length; j++) {
-                    // console.log(steps[j].step)
-                    setInstructions(instructions => [...instructions, steps[j].step])
-                }
-            }
-        }
-    }, [data])
+ 
 
 
     useEffect(() => {
         setId(match.params.id)
-        setUrl(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=${apiKey}`)
-        setMissedIngredients(location.state.missedIngredients)
+      setUrl(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=${apiKey}`)
     }, [match.params.id, id])
+
 
     useEffect(() => {
 
@@ -47,30 +34,39 @@ const SingleRecipe = (props) => {
                   const items = JSON.parse(result);
                   console.log(items)
                   setData(items);
-                //   setIsLoading(false);
                   setUrl('');
                   
         
                 })        
               } catch (error) {
                 console.log(error)
-                // setError(error);
-                // setIsLoading(false)
               }
             };
             if (url) {
                 fetchData();
             }
           }, [url]);
+
+          useEffect( () => {
+            if (data) {
+              // go over data and save the steps in state
+                for(let i = 0; i < data.length; i++) {
+                    const steps = data[i].steps
+                    for (let j=0; j<steps.length; j++) {
+                        setInstructions(instructions => [...instructions, steps[j].step])
+                    }
+                }
+            }
+        }, [data])
+
     return(
-        <>
+        <div className="wrapper recipe-page">
+        <h2>{state.title}</h2>
+        <img src={state.image} alt={state.title}/>
         <p>This will be a single recipe page</p>
         <Steps steps={instructions} />
-        {/* {steps => steps.map((step) => {
-            return <p>{step}</p>;
-        })} */}
         <Link to="/">Go Home</Link>
-        </>
+        </div>
     )
 }
 
